24059e8c6ccb5dbbfbb541f808f1733517ac26dd
Steven Carton <spatc@protonmail.com>
2021-02-26 02:34:11 -0500

    number guessing game finished, need to refactor

--- script.js
+++ script.js
@@ -13,6 +13,7 @@
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -32,6 +33,10 @@ document.querySelector('.check').addEventListener('click', function () {
 
     document.querySelector('.number').style.width = '30rem';
 
+    if (score > highscore) {
+      highscore = score;
+    }
+    document.querySelector('.highscore').textContent = score;
     // When guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
@@ -56,11 +61,18 @@ document.querySelector('.check').addEventListener('click', function () {
   }
 });
 document.querySelector('.again').addEventListener('click', function () {
+  // if (score > highscore) {
+  //   highscore = score;
+  // } else {
+  //   continue;
+  // }
+  // document.querySelector('.highscore').textContent = highscore;
   score = 20;
+
   const secretNumber = Math.trunc(Math.random() * 20) + 1;
   document.querySelector('.message').textContent = 'Start guessing...';
   document.querySelector('.score').textContent = score;
-  document.querySelector('.number').textContent = secretNumber;
+  document.querySelector('.number').textContent = '?';
   document.querySelector('.guess').value = '';
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').style.width = '15rem';
