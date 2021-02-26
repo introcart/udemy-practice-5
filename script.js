395c6fc098a8f1fea0797b057955c4de4cfd9f11
Steven Carton <spatc@protonmail.com>
2021-02-25 21:08:13 -0500

    game

--- script.js
+++ script.js
@@ -10,6 +10,21 @@
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('.guess').value);
 // console.log(document.querySelector('.guess').value);
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function () {
-  console.log(document.querySelector('.guess').value);
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔️ No number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = '📈 too High';
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = '📉 too Low!';
+  }
 });
